472d52b8f84500f2f32baed640b258cbb3d61cea
Perfect 7 <uriahrsanders@gmail.com>
2020-03-08 18:12:36 -0700

    Just getting started

---
+++ sasame.js
@@ -0,0 +1,105 @@
+/*
+Death by Bubbles Book 1
+Lead Author Uriah Sanders
+Ashka Productions: Uriah, Deszha, Key
+
+Esimode 1. My Favorite Moment.
+Esimode 2. Too Strong to Die.
+Esimode 3. The way we met.
+Esimode 4. Happy Valentine.
+*/
+
+/*
+Goals:
+1. Complete Book 1.
+2. Complete 'Long Journey'.
+3. Complete Sasame.
+4. Complete United Life.
+5. Complete E-App.
+*/
+
+//Goal 1
+var Book1 = function(){
+  var esimodes = {
+  'My Favorite Moment': function(){
+    var scene1 = `
+    They pull the bag off of Wolhal's face.
+    He is staring right into the face of Chromia Rea.
+    She's smiling at him.
+
+    "Here's a map of the facility," She says.
+    On all 4 sides it is marked 'Classified'.
+    But at the center of 'Resistance Headquarters',
+    he can see a map of offices forming a square.
+    And in the center is a courtyard of evergreen grass,
+    beneath which is a lake dressed with several bridges.
+    Lining the offices is the Golden Road Army.
+
+    "Are you ready for your interview?" She asks.
+    Wolhal nods his head.
+    The guards, coated in their Golden Armor,
+    step outside.
+
+    "What's your favorite color?" Asks Chromia Rea.
+    "Blue. What's yours?"
+    "I like rainbows. But if I had to be more specific I'd maybe say Pink."
+    "Nice!"
+
+    "Do you have a voice in your head?" Asks Chromia Rea.
+    "Yes."
+    "What's it say?"
+    "Well...it goes something like --"
+    And then Wolhal Ik-Song begins singing.
+
+    "What a beautiful song! You're an incredible singer." Says Chromia Rea.
+    "Thank you! No one has ever told me that before!
+    What does the voice in *your* head sound like?"
+    Chromia thinks for a moment. She blushes.
+    "Oh, I couldn't tell you."
+
+    *How cute!* Thinks Wolhal Ik-Song.
+
+    "Let's go for a walk." Says Chromia Rea.
+    And they step outside the office and into the Courtyard.
+    `;
+    var description = [scene1];
+  },
+  'Too Strong to Die', 
+  'The way we met', 
+  'Happy Valentine'
+  };
+  var Travelers = [
+  '#1',
+  'Elizabeth Lovelace',
+  'The Count',
+  'Little Red',
+  'Doopity Whoop',
+  'WOW',
+  'Princess',
+  'Buttface',
+  'Hitler',
+  ' ',
+  'WOL HAL IK SONG'
+  ];
+  var characters = [
+  ['David', 'Wolhal Ik-Song'],
+  ['Chromia Rea', 'Taelon'],
+  'Mango Pinklove',
+  'Chrystheria Pinklove',
+  'Valentine Pinklove',
+  'Marble Pinklove',
+  ['Uriah', 'Flair'],
+  'Akira Rea',
+  'Doopity Whoop',
+  'Little Red'
+  ];
+
+};
+//Goal 2
+
+//Goal 3
+
+//Goal 4
+
+//Goal 5
+
